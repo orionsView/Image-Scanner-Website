997818281a3c096b22b1d24f7717ff2eb65d5979
orionsView <davidhunterphillips03@gmail.com>
2025-03-25 16:27:35 -0700

    intital commit

---
+++ sandbox/sandScript.js
@@ -0,0 +1,82 @@
+// window.onload=getExif;
+
+let fnums = new Map([
+    [1.0, 0], [1.1, 0], [1.2, 0], [1.4, 0], [1.6, 0], [1.8, 0], [2, 0], [2.2, 0], [2.5, 0], [2.8, 0],
+    [3.2, 0], [3.5, 0], [4, 0], [4.5, 0], [5.0, 0], [5.6, 0], [6.3, 0], [7.1, 0], [8, 0], [9, 0],
+    [10, 0], [11, 0], [13, 0], [14, 0], [16, 0], [18, 0], [20, 0], [22, 0], [25, 0], [29, 0],
+    [32, 0], [36, 0], [40, 0], [45, 0], [51, 0], [57, 0], [64, 0], [72, 0], [81, 0], [90, 0],
+    [102, 0], [114, 0], [128, 0]
+])
+
+
+
+document.getElementById("fileInput").addEventListener("change", function (event) {
+    const lab = document.getElementById("l");
+    const folder = event.target.files;
+    lab.innerHTML = "";//clear
+
+    let processedFiles = 0;
+    let totalFiles = folder.length;
+
+    for (let file of folder) {
+        EXIF.getData(file, function () {
+            var metaData = EXIF.getTag(this, "FNumber");
+            if (metaData != undefined) {
+                fnums.set(metaData.valueOf(), fnums.get(metaData.valueOf()) + 1);
+            }
+            processedFiles++;
+
+            // lab.textContent = "hello";
+            console.log(processedFiles);
+            if (processedFiles === totalFiles) {
+                console.log(processedFiles);
+
+                const labels = Array.from(fnums.keys());
+                const vals = Array.from(fnums.values());
+
+                // console.log("flables: ", labels);
+                // console.log("fval: ", vals);
+                updateChart();
+            }
+        });
+    }
+
+})
+
+
+function updateChart() {
+    console.log(fnums);
+    let labels = Array.from(fnums.keys());
+    labels.length = 20;
+    const values = Array.from(fnums.values());
+
+    const c1 = document.getElementById("chart1");
+    new Chart(c1, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: '# of Votes',
+                data: values,
+                borderWidth: 1,
+                // backgroundColor: "rgba(75, 192, 192, 0.6)"
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
+//ExposureTime
+//FocalLength
+//ISOSpeedRatings
+//FNumber
+
+// document.addEventListener("DOMContentLoaded", function () {
+
+// });
