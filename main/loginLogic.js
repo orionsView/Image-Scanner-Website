882a228656420ded44c07e8547bdf97e9b10fa38
David P <davidhunterphillips03@gmail.com>
2025-04-07 19:47:21 -0700

    login page now checks if user exists before opening new page

--- main/loginLogic.js
+++ main/loginLogic.js
@@ -10,16 +10,31 @@ document.getElementById("subButton").addEventListener("click", function (event) 
 
 function checkUserName(userName) {
     let header = document.getElementById("topText");
-    if (userName === "bob_w") {
-        console.log("correct");
-        //go to alternate page
-        localStorage.setItem("userName", userName); // store the value in local storage
-        window.location.href = "UserHomePage.html";
-    } else {
-        console.log("incorrect");
-        header.textContent = "Incorrect Username";
-    }
+    userNameExistsCheck(userName).then(userExists => {
+        if (userExists) {
+            console.log("correct");
+            //go to alternate page
+            localStorage.setItem("userName", userName); // store the value in local storage
+            window.location.href = "UserHomePage.html";
+        } else {
+            console.log("incorrect");
+            header.textContent = "Incorrect Username";
+        }
+    })
 
 }
+
+async function userNameExistsCheck(userName) {
+    const urlToFetch = `http://localhost:5000/api/userexists/${userName}`;
+    try {
+        const response = await fetch(urlToFetch);
+        const data = await response.json();
+        return data && data.length > 0;
+    } catch (error) {
+        console.error('Error checking username:', error);
+        return false;
+    }
+}
+
 
 // export {userName};
